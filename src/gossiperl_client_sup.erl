69400879a61ab860682b147b1d80ca090013910d
Hampus <hampus.bergstrom@hotmail.com>
2016-08-19 14:38:22 +0200

    DEBUG

--- src/gossiperl_client_sup.erl
+++ src/gossiperl_client_sup.erl
@@ -1,27 +1,5 @@
-%% Copyright (c) 2014 Radoslaw Gruchalski <radek@gruchalski.com>
-%%
-%% Permission is hereby granted, free of charge, to any person obtaining a copy
-%% of this software and associated documentation files (the "Software"), to deal
-%% in the Software without restriction, including without limitation the rights
-%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-%% copies of the Software, and to permit persons to whom the Software is
-%% furnished to do so, subject to the following conditions:
-%%
-%% The above copyright notice and this permission notice shall be included in
-%% all copies or substantial portions of the Software.
-%%
-%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-%% THE SOFTWARE.
-
 -module(gossiperl_client_sup).
-
 -behaviour(supervisor).
-
 -export([start_link/0, init/1]).
 -export([ connect/1,
           disconnect/1,
@@ -42,8 +20,8 @@ init([]) ->
   ETS = ets:info(?CONFIG_ETS),
   io:format("ETS: ~p ~n ~n", [ETS]),
 
-  gossiperl_log:info("Gossiperl client application running."),
-  {ok, {{one_for_all, 10, 10}, [{
+%  gossiperl_log:info("Gossiperl client application running."),
+  {ok, {{one_for_all, 10, 60}, [{
     gossiperl_client_serialization,
     {gossiperl_client_serialization, start_link, []},
     permanent,
